11e21ceb837580786ee0a5ec103887ec1c281c43
Asfer Ali <asferali004@gmail.com>
2023-04-26 19:08:49 +0500

    made the layout of services page

--- src/pages/ServicesPage/ServicesPage.jsx
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -3,10 +3,22 @@ import services from "../../data/services";
 
 const Service = ({ heading, image, text }) => {
   return (
-    <div>
-      <img src={image} alt="" />
-      <h2 className="text-4xl font-medium">{heading}</h2>
-      <p className="text-zinc-600">{text}</p>
+    <div className="mb-16">
+      <img className="mx-auto" src={image} alt={"service-" + heading} />
+      <h2 className="text-4xl font-medium mt-12">{heading}</h2>
+      <p className="text-zinc-600 mt-4">
+        {text}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        inventore iste repellendus, quibusdam optio autem nobis! Quam dicta
+        blanditiis animi? Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Autem adipisci illum nisi quasi consequatur ea totam quia
+        necessitatibus ab dicta. <br />
+        <br />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus animi
+        ipsum ab exercitationem error tenetur quo, explicabo, ducimus alias
+        quibusdam eos! Animi odio saepe, sunt nulla est enim id ab ipsam optio
+        tempora iure iste atque numquam debitis illum quis.
+      </p>
     </div>
   );
 };
@@ -16,14 +28,13 @@ const ServicesPage = ({ scrollUp }) => {
   return (
     <div className="container-default mt-28">
       <h1 className="text-5xl md:text-7xl font-semibold">Our Services</h1>
-      <p className="text-zinc-600 w-2/3">
+      <p className="text-zinc-600 md:w-2/3">
         We provide a variety of luxury ride services for you to choose from.
-        They go from intercity, rides to, wedding events to business events and
-        much more. Below are all the services we provide with all the detail you
-        need.
+        Whether you want intercity rides or a wedding event, we've got you
+        covered.
       </p>
 
-      <div>
+      <div className="mt-20">
         {services.map((service) => (
           <Service {...service} />
         ))}
